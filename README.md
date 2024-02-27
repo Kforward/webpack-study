7219a6a3e96272b0bbcc6e15f7bcd2abe73ce161
k <67428794+Kforward@users.noreply.github.com>
2024-02-27 10:11:54 +0800

    feat: webpack - 管理输出

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # 学习 webpack
 
-> webpack - 基本状态
+> webpack - 管理输出
+
+`HtmlWebpackPlugin`
+会修改 `index.html` 文件中内容
+```js
+ plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出'
+    })
+  ],
+```
+`clean: true`
+
+每次构建时，清空dist目录内容
+
+```js
+output: {
+    clean: true
+  },
+```
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from "lodash";
+import printMe from "./print";
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button")
 
   element.innerHTML = _.join(["hello", "webpack"], " ");
 
+  btn.innerHTML = '点击我并查看控制台'
+  btn.onclick = printMe
+
+  element.appendChild(btn)
   return element
 }
 
